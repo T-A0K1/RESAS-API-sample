44ca69420732762097c0c249ca509eaeac19e7cf
T-A0K1 <liondrop409@yahoo.co.jp>
2022-06-17 10:08:46 +0900

    First Commit

---
+++ GetPrefPopulation/main.py
@@ -0,0 +1,43 @@
+import json
+import urllib.request
+import pandas as pd
+import sys
+
+def main(output_filepath, prefCode, cityCode = '-'):
+    # prefCode: 1~47 ALLの場合は全都道府県
+    # cityCode: 全市町村：-
+    
+    # SET PARAMS
+    output_filepath = 'download/population_composition_perYear.csv'
+    with open('setting/api_key_sample.json') as f:
+        api_key = json.load(f)
+    df_output = pd.DataFrame()
+    
+    # ALL処理
+    if prefCode == 'ALL':
+        prefCode_list = range(1,48)
+    else:
+        prefCode_list = [prefCode]  
+    
+    for prefCodeTmp in prefCode_list:
+        print(f'prefCode={prefCodeTmp} start', end=', ')
+        url = f'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode={cityCode}&prefCode={prefCodeTmp}'
+
+        # GET DATA
+        req = urllib.request.Request(url, headers=api_key)
+        with urllib.request.urlopen(req) as response:
+            data = response.read()
+            
+        # EDIT DATA
+        d = json.loads(data.decode())['result']['data']
+        for i in range(len(d)):
+            df_tmp = pd.DataFrame(d[i]['data'])
+            df_tmp['population_type'] = d[i]['label']
+            df_tmp['prefCode'] = prefCodeTmp
+            df_output = pd.concat([df_output, df_tmp])
+        df_output = df_output.loc[:,['prefCode', 'population_type', 'year', 'value', 'rate']]
+    
+    df_output.to_csv(output_filepath, index=False)
+    
+if __name__ == "__main__":
+    sys.exit(main('download/population_composition_perYear.csv', 'ALL'))
---
+++ README.md
@@ -0,0 +1,11 @@
+## 概要
+RESAS APIをpythonで使用する場合のサンプルです。  
+APIの仕様は、公式をご確認ください。  
+https://opendata.resas-portal.go.jp/docs/api/v1/index.html
+
+## 準備
+本レポジトリをpull後に、settingディレクトリにapi_key_sample.jsonファイルを作成し、以下を入力ください。
+> {"X-API-KEY":"YOUR API KEY"}
+
+※ "YOUR API KEY"はRESAS APIのHPからAPI KEYを事前に取得し、その値を記入。
+https://opendata.resas-portal.go.jp/form.html
